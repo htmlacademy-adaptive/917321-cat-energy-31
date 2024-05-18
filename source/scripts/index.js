56205e895f85a09a08b95707c702f1592571cbfa
Kseniya <48386878+DXenium@users.noreply.github.com>
2024-05-19 00:02:50 +0300

    Добавляет эффекты при наведении (#32)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,17 @@
 /* в этот файл добавляет скрипты*/
+const headerPage = document.querySelector('.main-header');
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-header__nav-toggle');
+
+headerPage.classList.remove('main-header--nojs');
+navMain.classList.add('main-nav--closed');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
